caeb4b33333a7420dcc07805b78858c6e8f97693
yunii23 <67429918+yunii23@users.noreply.github.com>
2023-03-23 01:12:45 +0900

    useState(), useEffect() 사용하여 데이터 요청 및 관리 완료

--- src/page/post/PostList.js
+++ src/page/post/PostList.js
@@ -1,44 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CommonTable from '../../component/table/CommonTable';
 import CommonTableColumn from '../../component/table/CommonTableColumn';
 import CommonTableRow from '../../component/table/CommonTableRow';
+import { postList } from "../../Data";
+
+// 컴포넌트 내부에서 state 변수로 사용될 dataList를 useState()를 통하여 생성해주고
+// useEffect()로 Component가 최초로 Render 되었을 때 위에서 생성해준 postList를 setDataList() 함수를 이용해
+// dataList의 값을 초기화 해주도록 함
+// 그리고 나서 dataList를 map() 함수를 활용해서 테이블 형태로 render 하도록 함
 
 const PostList = (props) => {
+    const [dataList, setDataList] = useState([]);
+
+    useEffect(() => {
+        setDataList(postList);
+    }, []);
+
     return (
         <>
             <CommonTable headersName={["글번호", "제목", "등록일", "조회수"]}>
-                <CommonTableRow>
-                    <CommonTableColumn>1</CommonTableColumn>
-                    <CommonTableColumn>첫번째 게시글입니다.</CommonTableColumn>
-                    <CommonTableColumn>2020-10-25</CommonTableColumn>
-                    <CommonTableColumn>6</CommonTableColumn>
-                </CommonTableRow>
-                <CommonTableRow>
-                    <CommonTableColumn>2</CommonTableColumn>
-                    <CommonTableColumn>두번째 게시글입니다.</CommonTableColumn>
-                    <CommonTableColumn>2020-10-25</CommonTableColumn>
-                    <CommonTableColumn>5</CommonTableColumn>
-                </CommonTableRow>
-                <CommonTableRow>
-                    <CommonTableColumn>3</CommonTableColumn>
-                    <CommonTableColumn>세번째 게시글입니다.</CommonTableColumn>
-                    <CommonTableColumn>2020-10-25</CommonTableColumn>
-                    <CommonTableColumn>1</CommonTableColumn>
-                </CommonTableRow>
-                <CommonTableRow>
-                    <CommonTableColumn>4</CommonTableColumn>
-                    <CommonTableColumn>네번째 게시글입니다.</CommonTableColumn>
-                    <CommonTableColumn>2020-10-25</CommonTableColumn>
-                    <CommonTableColumn>2</CommonTableColumn>
-                </CommonTableRow>
-                <CommonTableRow>
-                    <CommonTableColumn>5</CommonTableColumn>
-                    <CommonTableColumn>
-                        다섯번째 게시글입니다.
-                    </CommonTableColumn>
-                    <CommonTableColumn>2020-10-25</CommonTableColumn>
-                    <CommonTableColumn>4</CommonTableColumn>
-                </CommonTableRow>
+                {
+                    dataList ? dataList.map((item, index) => {
+                        return (
+                            <CommonTableRow key={index}>
+                                <CommonTableColumn>{item.no}</CommonTableColumn>
+                                <CommonTableColumn>{item.title}</CommonTableColumn>
+                                <CommonTableColumn>{item.createDate}</CommonTableColumn>
+                                <CommonTableColumn>{item.readCount}</CommonTableColumn>
+                            </CommonTableRow>
+                        )
+                    }) : ''
+                }
             </CommonTable>
         </>
     );
